69960ed50a4e8dfb624d2fb836d5ca78ed63b538
陈昕 <chen@huohua.cn>
2019-08-05 13:53:34 +0800

    vue 实现 Board

---
+++ 黑白棋项目/id_1908017/src/components/OthelloGame.vue
@@ -0,0 +1,36 @@
+<template>
+  <Board :cells="board" :cellClick="cellClick"></Board>
+</template>
+
+<script>
+import Board from "./Board";
+export default {
+  components: {
+    Board
+  },
+  data() {
+    return {
+      board: [
+        /* eslint-disable prettier/prettier */
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 2, 1, 0, 0, 0,
+            0, 0, 0, 1, 2, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0
+            ]
+        };
+    },
+    methods:{
+        cellClick(i){
+            console.log('cellClick',i);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
