7e2d42d88d560de7b5d372217a014f3d8dbb5d50
陈昕 <chen@huohua.cn>
2019-08-06 17:14:19 +0800

    拆分棋子组件，准备做翻转效果 样式改进，使用font-size 控制棋盘大小

---
+++ 黑白棋项目/id_1908017/src/components/Piece.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="cell"
+    :class="{ 1: 'piece-black', 2: 'piece-white' }[color]"
+    v-on="$listeners"
+  ></div>
+</template>
+<script>
+export default {
+  props: ["color"]
+};
+</script>
+
+<style scoped>
+.cell {
+  float: left;
+  height: 1em;
+  width: 1em;
+  background-color: darkgreen;
+  position: relative;
+  box-sizing: border-box;
+  border: 1px solid white;
+}
+.piece-black::after,
+.piece-white::after {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 70%;
+  height: 70%;
+  margin: auto;
+  border-radius: 50%;
+  background: white;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);
+}
+.piece-black::after {
+  background-color: black;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);
+}
+</style>
