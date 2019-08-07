e4d249b7d1672f5f00c351ec0482c6242b035293
zhangtj <1771988873@qq.com>
2019-08-07 12:46:27 +0800

    添加vue版的，AI部分后续再添加

---
+++ 黑白棋项目/id_1908022/othello-vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <Othello msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Othello from './components/Othello.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Othello
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
