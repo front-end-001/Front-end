25c068d82279f0836330797a500b0f0cee2b55bf
Quanquan <zhanglinling0926@gmail.com>
2019-08-07 12:18:01 +0800

    feat(黑白棋-vue): 创建vue项目模板
    feat(黑白棋-vue): 创建项目
    
    chore(ignore): 更新gitignore配置
    
    feat(黑白棋-vue): 删除项目初始内容, 添加黑白棋初始结构
    
    feat(黑白棋-vue): 更新readme

---
+++ 黑白棋项目/id_1908003/othello-vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <othello-game msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import OthelloGame from './components/OthelloGame.vue'
+
+export default {
+  name: 'app',
+  components: {
+    OthelloGame
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 黑白棋项目/id_1908003/othello-vue/src/components/OthelloGame.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div @click="count++">{{ count }}</div>
+  </div>
+</template>
+
+<script>
+// import { deepClone } from '@/assets/js/utils.js';
+
+export default {
+  name: 'OthelloGame',
+  data() {
+    return {
+      count: 0,
+      msg: 'Othello Games',
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
