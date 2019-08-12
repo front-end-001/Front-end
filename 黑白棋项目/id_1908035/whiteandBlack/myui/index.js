8b0536323c627151df11253c374f063b2973df95
zhangdongming <zhangdongming@zhihu.com>
2019-08-13 00:13:06 +0800

    Revert "黑白棋"
    This reverts commit 47da7285

---
+++ 黑白棋项目/id_1908035/whiteandBlack/myui/index.js
@@ -0,0 +1,22 @@
+function getItem(x,y,color){
+   var tdStr = ''
+    for(var i=0;i<8;i++){
+        tdStr+='<div class="item" data-x='+i+'></div>'
+
+    }
+    var trStr = ''
+    for(var j=0;j<8;j++){
+        trStr += '<div data-y='+j+'>'+tdStr+'</div>'
+    }
+    $('.box').html(trStr)
+    var whitePieces = '<div class="Whiteqizi"></div>'
+    var blackPieces = '<div class="blackqizi"></div>'
+    // $('[data-y='+y+']').find('[data-x = '+x+']').append(color)
+    $('[data-y=3]').find('[data-x = 3]').append(whitePieces)
+    $('[data-y=3]').find('[data-x = 4]').append(blackPieces)
+    $('[data-y=4]').find('[data-x = 3]').append(blackPieces)
+    $('[data-y=4]').find('[data-x = 4]').append(whitePieces)
+}
+getItem(3,4)      
+
+
