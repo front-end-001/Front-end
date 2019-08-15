8d37c5bbb54b67f0697f9a4141c7ac7eeac8adc8
zhangdongming <zhangdongming@zhihu.com>
2019-08-15 22:34:09 +0800

    feat: first part of carousel

---
+++ 组件开发项目/id_1908059/webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: "dist.js"
+  },
+  module: {
+    rules: [{
+      test: /\.js/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: ["@babel/plugin-proposal-class-properties"]
+      },
+    }, {
+      test: /\.css/,
+      use: ['style-loader', 'css-loader'],
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    })
+  ]
+}
