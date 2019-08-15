99f89eb43a4d585c4ac1fd9c10c9a817482ea127
Quanquan <zhanglinling0926@gmail.com>
2019-08-15 16:56:02 +0800

    feat(carousel): 添加基础文件

---
+++ 组件开发项目/id_1908003/carousel/app.js
@@ -0,0 +1,6 @@
+const imgList = [
+  '../carousel-vue/public/imgs/cat1.jpg',
+  '../carousel-vue/public/imgs/cat2.jpg',
+  '../carousel-vue/public/imgs/cat3.jpg',
+  '../carousel-vue/public/imgs/cat4.jpg',
+];
