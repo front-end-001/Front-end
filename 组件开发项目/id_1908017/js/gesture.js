1e3bdd078ce034790d1f7b8e946b162d28b52bbc
chenxin <lightnoway@qq.com>
2019-08-19 21:21:52 +0800

    分离各手势逻辑

--- 组件开发项目/id_1908017/js/gesture.js
+++ 组件开发项目/id_1908017/js/gesture.js
@@ -1,34 +1,56 @@
 function enableGesture(el) {
     const context = Object.create(null);
     const start = (point, context) => {
-        context.isTab = true;
+        context.moved = false;
+        context.pressStarted = false;
+        context.pressBeginTimer = setTimeout(() => {
+            context.pressStarted = true;
+        }, 500);
         context.startTime = +new Date();
-        context.isFlick = false;
+        // context.isFlick = false;
+        // context.isVertical = null;
         context.startX = point.clientX;
         context.startY = point.clientY;
     };
     const move = ({ clientX, clientY }, context) => {
         const dx = clientX - context.startX,
             dy = clientY - context.startY;
-        if (context.isTab) {
+
+        if (!context.moved) {
             if (dx * dx + dy * dy > 100) {
-                context.isTab = false;
-                const panstart = new Event('panstart');
-                panstart.startX = context.startX;
-                panstart.startY = context.startY;
-                el.dispatchEvent(panstart);
+                context.moved = true;
+                //press
+                if (!context.pressStarted) {
+                    clearTimeout(context.pressBeginTimer);
+                    delete context.pressBeginTimer;
+                } else {
+                    const e = new Event('presscancel');
+                    el.dispatchEvent(e);
+                }
+                //panstart
+                context.moved = Math.abs(dx) >= Math.abs(dy) ? 'x' : 'y';
+                const e = new Event('panstart');
+                e.startX = context.startX;
+                e.startY = context.startY;
+                e.isVertical = context.moved === 'y';
+                el.dispatchEvent(e);
             }
         } else {
-            const pan = new Event('pan');
-            pan.dx = dx;
-            pan.dy = dy;
-            el.dispatchEvent(pan);
+            //pan 
+            const e = new Event('pan');
+            e.dx = dx;
+            e.dy = dy;
+            e.isVertical = context.moved === 'y';
+            el.dispatchEvent(e);
         }
     };
     const end = ({ clientX, clientY }, context) => {
-        if (context.isTab) {
-            const tab = new Event('tab');
-            el.dispatchEvent(tab);
+        if ('pressBeginTimer' in context) {
+            clearTimeout(context.pressBeginTimer);
+        }
+        if (!context.moved) {
+            const e = new Event(!context.pressStarted ? 'tab' : 'pressend');
+            el.dispatchEvent(e);
         } else {
             const dx = clientX - context.startX,
                 dy = clientY - context.startY;
@@ -50,15 +72,21 @@ function enableGesture(el) {
         }
     };
     const cancel = (event, context) => {
-        if (context.isTab) {
-            return;
+        if ('pressBeginTimer' in context) {
+            clearTimeout(context.pressBeginTimer);
+        }
+        if (!context.moved) {
+            if (pan)
+
+                return;
         } else {
             const dx = clientX - context.startX,
                 dy = clientY - context.startY;
-            const pancancel = new Event('pancancel');
-            pancancel.dx = dx;
-            pancancel.dy = dy;
-            el.dispatchEvent(pancancel);
+            const e = new Event('pancancel');
+            e.dx = dx;
+            e.dy = dy;
+            e.isVertical = context.moved === 'y';
+            el.dispatchEvent(e);
         }
     }
 
@@ -108,3 +136,30 @@ function enableGesture(el) {
     el.addEventListener('touchend', touchend);
     el.addEventListener('touchcancel', touchcancel);
 }
+//todo 换 isTab 风格再实现一遍
+//?? isTab 风格有重复么？
+/**
+ * test
+ * 
+ * tap: 
+ *  start(0,0).then(time:100).then(end(0,0))
+ * pressstart: 
+ *  start(0,0).then(time:500).then(end(0,0))
+ * presscancel:
+ *  start(0,0).then(time:500).then(move(10,0))
+ * pressend:
+ *  start(0,0).then(time:500).then(end(0,0))
+ * 
+ * panstart:
+ *  start(0,0).then(move(10,0))
+ * pan:
+ *  start(0,0).then(move(10,0))
+ * panend:
+ *  start(0,0).then(move(10,0)).then(end())
+ *   
+ * pancancel:
+ *  start(0,0).then(move(10,0)).then(cancel())
+ * 
+ * flick:
+ *  start(0,0).then(move(10,0)).then(end())
+ */
