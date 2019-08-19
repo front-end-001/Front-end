ac59a5211a381f30c0f3313de0bbc93880349a07
zhangtj <1771988873@qq.com>
2019-08-20 07:24:34 +0800

    test

--- 组件开发项目/id_1908022/slideshow/gesture.js
+++ 组件开发项目/id_1908022/slideshow/gesture.js
@@ -4,16 +4,44 @@ function enableGesture(main){
         context.startX = point.clientX;
         context.startY = point.clientY;
 
+        context.startTime = Date.now();
+
         context.isTap = true;
         context.isPan = false;
+        context.isPress = false;
+        context.pressHandler = setTimeout( () => {
+            context.isPress = true;
+            context.isTap = false;
+            let e = new Event("pressstart");
+            main.dispatchEvent(e);
+            context.pressHandler = null;
+        }, 500)
     }
     let move = (point, context) => {
         //console.log(context.startX,context.startY);
         let dx = point.clientX - context.startX, dy = point.clientY - context.startY;
         if(dx * dx + dy * dy > 100) {
+            
+            if(context.pressHandler !== null) {
+                clearTimeout(context.pressHandler);
+                context.pressHandler = null;
+                context.isPress = false;
+            } else if(context.isPress){
+                context.isPress = false;
+                let e = new Event("presscancel");
+                main.dispatchEvent(e);
+            }
+
             context.isTap = false;
             
             if(context.isPan == false) {
+                if(Math.abs(dx) > Math.abs(dy)) {
+                    context.isVertical = false;
+                    context.isHorizontal = true;
+                } else {
+                    context.isVertical = true;
+                    context.isHorizontal = false;
+                }
                 let e = new Event("panstart");
                 e.startX = context.startX;
                 e.startY = context.startY;
@@ -22,24 +50,48 @@ function enableGesture(main){
             }
         }
         if(context.isPan) {
-            let e = new Event("panmove");
+            let e = new Event("pan");
             e.dx = dx;
             e.dy = dy;
+            e.isHorizontal = context.isHorizontal;
+            e.isVertical = context.isVertical;
             main.dispatchEvent(e);
         }
         //console.log(dx , dy);
     }
     let end = (point, context) => {
         //console.log("end")
+        if(context.pressHandler !== null) {
+            clearTimeout(context.pressHandler);
+        }
+        if(context.isPress) {
+            let e = new Event("pressend");
+            main.dispatchEvent(e);
+        }
         if(context.isTap) {
             let e = new Event("tap");
             main.dispatchEvent(e);
         }
-        if(context.isPan) {
-            let e = new Event("panend");
-            let dx = point.clientX - context.startX, dy = point.clientY - context.startY;
+        let dx = point.clientX - context.startX, dy = point.clientY - context.startY;
+
+        let v = Math.sqrt(dx * dx + dy * dy) / (Date.now() - context.startTime);
+        //console.log(v);
+        if(context.isPan && v > 0.3) {
+            context.isFlick = true;
+            let e = new Event("flick");
             e.dx = dx;
             e.dy = dy;
+            main.dispatchEvent(e);
+        } else {
+            context.isFlick = false;
+        }
+        if(context.isPan) {
+            let e = new Event("panend");
+            e.dx = dx;
+            e.dy = dy;
+            e.isFlick = context.isFlick;
+            e.isHorizontal = context.isHorizontal;
+            e.isVertical = context.isVertical;
             main.dispatchEvent(e);
         }
     }
@@ -47,6 +99,15 @@ function enableGesture(main){
         if(context.isPan) {
             let e = new Event("pancancel");
             main.dispatchEvent(e);
+        }
+        if(context.isPress) {
+            let e = new Event("presscancel");
+            main.dispatchEvent(e);
+        }
+        if(context.pressHandler !== null) {
+            let e = new Event("pancancel");
+            main.dispatchEvent(e);
+            clearTimeout(context.pressHandler);
         }
     }
     let contexts = Object.create(null);
---
+++ 组件开发项目/id_1908022/slideshow/touch.js
@@ -0,0 +1,107 @@
+function enableGesture(main){
+    let contexts = Object.create(null);
+    
+    let start = (point,context) => {
+        // console.log('start',point.clientX,point.clientY);
+
+        context.startX = point.clientX;
+        context.startY = point.clientY;
+        context.isTap = true;
+        context.isPan = false;
+    }
+    let move = (point,context) => {
+        // console.log('move',point.clientX,point.clientY);
+        let dX = point.clientX - context.startX , dY = point.clientY - context.startY;
+        if((dX * dX + dY * dY) > 100){
+            context.isTap = false;
+            if(!context.isPan){
+                let e = new Event('panstart');
+                e.startX = context.startX;
+                e.startY = context.startY;
+                main.dispatchEvent(e);
+                context.isPan = true;
+            }
+        }
+        if(context.isPan){
+            let e = new Event('panmove');
+            e.dX = dX;
+            e.dY = dY;
+            main.dispatchEvent(e);
+        }
+    }
+    let end = (point,context) => {
+        // console.log('end',point.clientX,point.clientY);
+        if(context.isTap){
+            let e = new Event('tap');
+            main.dispatchEvent(e);
+        }
+            
+        if(context.isPan){
+            let e = new Event('panend');
+            let dX = point.clientX - context.startX , dY = point.clientY - context.startY;
+            e.dX = dX;
+            e.dY = dY;
+            main.dispatchEvent(e);
+        }
+            
+    }
+
+    let mouseSymbol  = Symbol('mouse'); 
+    let mouseDown = event => {
+        main.addEventListener('mousemove',mouseMove);
+        main.addEventListener('mouseup',mouseUp);
+        contexts[mouseSymbol] = Object.create(null);
+        start(event, contexts[mouseSymbol]);
+    }
+    let mouseMove = event => {
+        move(event, contexts[mouseSymbol]);
+    }
+    let mouseUp = event => {
+        main.removeEventListener('mousemove',mouseMove);
+        main.removeEventListener('mouseup',mouseUp);
+        end(event, contexts[mouseSymbol]);
+    }
+    main.addEventListener('mousedown',mouseDown);
+
+    let touchStart = event => {
+        for(let point of event.changedTouches){
+            contexts[point.identifier] = Object.create(null);
+            start(point,contexts[point.identifier]);
+        }
+    }
+    let touchMove = event => {
+        for(let point of event.changedTouches){
+            move(point,contexts[point.identifier]);
+        }
+    }
+    let touchEnd = event => {
+        for(let point of event.changedTouches){
+            end(point,contexts[point.identifier]);
+            delete contexts[point.identifier];
+        }
+    }
+    let touchCancle = event => {
+        for(let point of event.changedTouches){
+            end(point,contexts[point.identifier]);
+            delete contexts[point.identifier];
+        }
+    }
+    main.addEventListener('touchstart',touchStart);
+    main.addEventListener('touchmove',touchMove);
+    main.addEventListener('touchend',touchEnd);
+    main.addEventListener('touchcancle',touchCancle);
+   /*  main.addEventListener('panstart', e => {
+        console.log('panstart',e);
+    });
+    main.addEventListener('panmove', e => {
+        console.log(e);
+        main.style.transform = `translate(${e.dX}px,${e.dY}px)`;
+    });
+    document.addEventListener('panend', e => {
+        
+    })
+    main.addEventListener('tap', e => {
+        console.log('tap',e);
+    }) */
+
+}
