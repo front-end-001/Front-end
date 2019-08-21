d5b3c8ad34dd73d50b015bb208c3a349568773b5
Hushin <874797577@qq.com>
2019-08-22 03:39:38 +0800

    封装手势库gesture.js

---
+++ 组件开发项目/id_1908051/gesture.js
@@ -0,0 +1,101 @@
+function enableGesture(main) {
+    let start = (point, context) => {
+        console.log('start');
+        context.startX = point.clientX;
+        context.startY = point.clientY;
+
+        context.isTap = true;
+        context.isPan = false;
+    }
+    let move = (point, context) => {
+        console.log('move');
+        let dx = point.clientX - context.startX;
+        let dy = point.clientY - context.startY;
+        if (dx * dx + dy * dy > 100) {
+            context.isTap = false;
+            if (!context.isPan) {
+                let e = new Event('panstart');
+                main.dispatchEvent(e);
+                e.startX = context.startX;
+                e.startY = context.startY;
+                context.isPan = true;
+            }
+        }
+        if (context.isPan) {
+            let e = new Event('pan');
+            e.dx = dx;
+            e.dy = dy;
+            main.dispatchEvent(e);
+        }
+    }
+    let end = (point, context) => {
+        if (context.isTap) {
+            let e = new Event('tap');
+            main.dispatchEvent(e);
+        }
+        if (context.isPan) {
+            let e = new Event('panend');
+            e.dx = point.clientX - context.startX;
+            e.dy = point.clientY - context.startY;
+            main.dispatchEvent(e);
+        }
+        console.log('end');
+    }
+    let cancel = (point, context) => {
+        if (context.isPan) {
+            let e = new Event('pancancel');
+            main.dispatchEvent(e);
+        }
+    }
+    
+    let contexts = Object.create(null);
+    let mouseSymbol = Symbol('mouse');
+    let mousedown = event => {
+        event.preventDefault();
+        document.addEventListener('mousemove', mousemove);
+        document.addEventListener('mouseup', mouseup);
+        contexts[mouseSymbol] = Object.create(null);
+        start(event, contexts[mouseSymbol]);
+    }
+    let mousemove = event => {
+        event.preventDefault();
+        move(event, contexts[mouseSymbol]);
+    }
+    let mouseup = event => {
+        document.removeEventListener('mousemove', mousemove);
+        document.removeEventListener('mouseup', mouseup);
+        end(event, contexts[mouseSymbol]);
+        delete contexts[mouseSymbol];
+    }
+    main.addEventListener('mousedown', mousedown);
+
+    let touchstart = event => {
+        event.preventDefault();
+        for (let touch of event.changedTouches) {
+            contexts[touch.identifier] = Object.create(null);
+            start(touch, contexts[touch.identifier]);
+        }
+    }
+    let touchmove = event => {
+        event.preventDefault();
+        for (let touch of event.changedTouches) {
+            move(touch, contexts[touch.identifier]);
+        }
+    }
+    let touchend = event => {
+        for (let touch of event.changedTouches) {
+            end(touch, contexts[touch.identifier]);
+            delete contexts[touch.identifier];
+        }
+    }
+    let touchcancel = event => {
+        for (let touch of event.changedTouches) {
+            cancel(touch, contexts[touch.identifier]);
+            delete contexts[touch.identifier];
+        }
+    }
+    main.addEventListener('touchstart', touchstart);
+    main.addEventListener('touchmove', touchmove);
+    main.addEventListener('touchend', touchend);
+    main.addEventListener('touchcancel', touchcancel);
+}
