947fa43d08c29bb87cd431c4febbf27e5071744c
shifeng-lee <lishibohao@163.com>
2019-08-22 23:17:01 +0800

    轮播组件

--- 组件开发项目/id_1908003/carousel/app.js
+++ 组件开发项目/id_1908003/carousel/app.js
@@ -1,8 +1,8 @@
 const imgList = [
-  '../carousel-vue/public/imgs/cat1.jpg',
-  '../carousel-vue/public/imgs/cat2.jpg',
-  '../carousel-vue/public/imgs/cat3.jpg',
-  '../carousel-vue/public/imgs/cat4.jpg',
+  '../carousel-step1-vue/public/imgs/cat1.jpg',
+  '../carousel-step1-vue/public/imgs/cat2.jpg',
+  '../carousel-step1-vue/public/imgs/cat3.jpg',
+  '../carousel-step1-vue/public/imgs/cat4.jpg',
 ];
 
 class Carousel {
