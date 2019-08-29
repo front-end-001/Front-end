3ce76ba065114c6ca8f2f543833fba3fcdd0aa40
chenxin <lightnoway@qq.com>
2019-08-29 22:32:48 +0800

    carousel  重构     CarouselItemRender 负责itemRender      改用 animation实现动画 animation fix 支持负数

--- 组件开发项目/id_1908017/js/carousel.js
+++ 组件开发项目/id_1908017/js/carousel.js
@@ -1,164 +1,189 @@
+const autoMoveDuration = 2000;
+const duration = 1500;
 class Carousel {
-    constructor({ el, items }) {
-        this._el = el;
-        this._items = items;
-        this.initDom();
-        this.pos = 0;
-        enableGesture(el);
-        // this.autoMove();
-        // this.enableAutoMoveStop();
-        this.setGesture();
-    }
+  constructor({ el, items }) {
+    this._el = el;
+    this._items = items;
+    this.initDom();
+    this.pos = 0;
+    enableGesture(el);
+    this.autoMove = this.autoMove.bind(this);
+    this.autoMoveTimer = setTimeout(this.autoMove, autoMoveDuration);
+    this.enableAutoMoveStop();
+    this.setGesture();
+  }
 
-    initDom() {
-        const { _el: el, _items: item } = this;
-        el.classList.add('carousel');
-        for (const pic of this._items) {
-            const img = new Image();
-            img.src = pic;
-            el.appendChild(img);
-        }
-    }
+  initDom() {
+    const { _el: el, _items: items } = this;
+    el.classList.add('carousel');
+    this._renders = items.map((pic, i) => {
+      const img = new Image();
+      img.src = pic;
+      el.appendChild(img);
+      return new CarouselItemRender(img, -i);
+    })
+  }
 
-    autoMove() {
-        this.autoMoveTimer = setTimeout(() => {
-            const nextPos = (this.pos + 1) % this._items.length;
-            this.move(this.pos, nextPos);
-            this.autoMove();
-        }, 1000);
-    }
+  autoMove() {
+    this.move(this.pos, this.pos + 1);
+    this.autoMoveTimer = setTimeout(this.autoMove, autoMoveDuration);
+  }
 
-    /**
-     * hover时停止autoMove
-     */
-    enableAutoMoveStop() {
+  /**
+   * hover时停止autoMove
+   */
+  enableAutoMoveStop() {
+    //考虑 与 hover 冲突: 并集（hover|| 手势）时停止 
+    let count = 0;
+    const stopAuto = () => {
+      count++;
+      log('count ++', count);
+      clearTimeout(this.autoMoveTimer);
+    };
+    const startAuto = () => {
+      count--;
+      log('count --', count);
+      count === 0 && (this.autoMoveTimer = setTimeout(this.autoMove, autoMoveDuration));
+    };
+    const el = this._el;
+    el.addEventListener('mouseenter', () => {
+      console.log('mouseenter');
+      stopAuto();
+    })
+    el.addEventListener('mouseleave', () => {
+      console.log('mouseleave');
+      startAuto();
+    })
 
-        // todo 考虑 与 hover 冲突？ 并集（hover|| 手势）时停止 
-        let count = 0;
-        const stopAuto = () => {
-            count++;
-            log('count ++', count);
-            clearTimeout(this.autoMoveTimer);
-        };
-        const startAuto = () => {
-            count--;
-            log('count --', count);
-            count === 0 && this.autoMove();
-        };
-        const el = this._el;
-        el.addEventListener('mouseenter', () => {
-            console.log('mouseenter');
-            stopAuto();
-        })
-        el.addEventListener('mouseleave', () => {
-            console.log('mouseleave');
-            startAuto();
-        })
+    //手势开始是停止计时器
+    el.addEventListener('start', () => stopAuto());
+    el.addEventListener('end', () => startAuto());
+    el.addEventListener('cancel', () => startAuto());
+  }
 
-        //手势开始是停止计时器
-        el.addEventListener('start', () => stopAuto());
-        el.addEventListener('end', () => startAuto());
-        el.addEventListener('cancel', () => startAuto());
-    }
-
-    setGesture() {
-        const el = this._el;
-        //点击跳转
-        el.addEventListener('tap', () => {
-            console.log('tap', this.pos); //跳转到pos
-        });
-        el.addEventListener('pan', e => {
-            // console.log('pan', e.dx);
-            this.update(e.dx);
-        });
-
-        //flick 左滑右滑
-        el.addEventListener('panend', e => {
-            if (e.isFlick) {
-                const r2l = e.dx < 0;
-                this.move(this.pos, this.getValidPos(this.pos + (r2l ? 1 : -1)), r2l);
-            } else {
-                const floatPos = this._getFloatPos(e.dx);
-                const aroundPos = Math.round(this._getFloatPos(e.dx));
-                const r2l = aroundPos > floatPos;
-                this.move(this.getValidPos(aroundPos + (r2l ? -1 : 1)), this.getValidPos(aroundPos), r2l);
-            }
-            // //todo 配合 手势 一开始就是高速
-        });
-        //todo flick优化 ,快速左右移动时 抖动 
-        //禁止拖拽图片
-        el.addEventListener('mousedown', e => e.preventDefault());
-        el.addEventListener('touchstart', e => e.preventDefault());
-    }
-    getValidPos(pos) {
-        return (pos + this._items.length) % this._items.length;
-    }
-    _moving(poses) {
-        const last = this._movingPos || [];
-        this._movingPos = poses;
-        for (const lastPos of last) {
-            this._el.children[lastPos].style.visibility = 'hidden';
-        }
-        for (const nPos of poses) {
-            this._el.children[nPos].style.visibility = '';
-        }
-        console.log('hide', last, 'show', poses);
-        return last;
-    }
-    _getFloatPos(dx) {
-        const containerWidth = this._el.offsetWidth;
-        const floatPos = this.pos - dx / containerWidth;
-        return floatPos;
-    }
-    update(dx) {
-        const floatPos = this._getFloatPos(dx);
-        const dotPos = floatPos - Math.floor(floatPos);
-        const leftPos = this.getValidPos(Math.floor(floatPos));
-        const rightPos = this.getValidPos(Math.ceil(floatPos));
-        console.log('floatPos:', floatPos, leftPos, rightPos);
-        const leftItem = this._el.children[leftPos];
-        this._moving([leftPos, rightPos]);
-        cssUpdate(leftItem, `${(-dotPos - leftPos) * 100}%`);
-        if (rightPos !== leftPos) {
-            const rightItem = this._el.children[rightPos];
-            cssUpdate(rightItem, `${(1 - dotPos - rightPos) * 100}%`);
-            //todo 考虑 移走之前的
-        }
-    }
-
-    move(leave, enter, r2l = true) {
-        console.log('leave,enter', leave, enter, r2l);
-        const leaveItem = this._el.children[leave];
-        const enterItem = this._el.children[enter];
-        this.pos = enter;
-        const lastMove = this._moving([leave, enter]);
-        //render
-        cssMove(leaveItem, `${(-leave) * 100}%`, `${((r2l ? -1 : 1) - leave) * 100}%`, lastMove.indexOf(leave) > -1);
-        cssMove(enterItem, `${((r2l ? 1 : -1) - enter) * 100}%`, `${(-enter) * 100}%`, lastMove.indexOf(enter) > -1);
-    }
-}
-
-function cssMove(dom, from, to, continuous = false) {
-    console.log('cssMove:', continuous);
-
-    if (!continuous) {
-        dom.classList.toggle('animate', false);
-        dom.style.transform = `translateX(${from})`;
-    }
-    requestAnimationFrame(() => {
-        requestAnimationFrame(() => {
-            dom.classList.toggle('animate', true);
-            dom.style.transform = `translateX(${to})`;
-        });
+  setGesture() {
+    const el = this._el;
+    //点击跳转
+    el.addEventListener('tap', () => {
+      console.log('jump to ', this.pos); //跳转到pos
     });
-    //todo：考虑 continuous时 requestAnimationFrame调用导致  被拉开距离了
+    el.addEventListener('pan', e => {
+      // console.log('pan', e.dx);
+      this.update(e.dx);
+    });
+
+    //flick 左滑右滑
+    el.addEventListener('panend', e => {
+      const fromPos = this._getFloatPos(e.dx);
+      let toPos
+      if (e.isFlick) {
+        console.log('flick', e.dx);
+        if (e.dx > 0) {
+          toPos = Math.ceil(fromPos - 1);
+        } else {
+          toPos = Math.floor(fromPos + 1);
+        }
+      } else {
+        toPos = Math.round(this._getFloatPos(e.dx));
+      }
+      this.move(fromPos, toPos);
+    });
+    //todo flick优化 ,快速左右移动时 抖动 
+    //禁止拖拽图片
+    el.addEventListener('mousedown', e => e.preventDefault());
+    el.addEventListener('touchstart', e => e.preventDefault());
+  }
+  getValidPos(pos) {
+    const len = this._items.length;
+    pos = pos % len;
+    if (pos < 0) {
+      pos += len;
+    }
+    return pos;
+  }
+  _getFloatPos(dx) {
+    const containerWidth = this._el.offsetWidth;
+    const floatPos = this.pos - dx / containerWidth;
+    return parseFloat(floatPos.toFixed(2));
+  }
+  update(dx) {
+    const floatPos = this._getFloatPos(dx);
+    const leftPos = Math.floor(floatPos);
+    const rightPos = Math.ceil(floatPos);
+    const [leftVPos, rightVPos] = [leftPos, rightPos].map(pos => this.getValidPos(pos));
+    this._stopLast([leftVPos, rightVPos]);
+    // console.log('update:', floatPos, [leftVPos, leftPos - floatPos], [rightVPos, rightPos - floatPos])
+    this._renders[leftVPos].show(leftPos - floatPos);
+    this._renders[rightVPos].show(rightPos - floatPos);
+  }
+
+  move(from, to) {
+    console.log('move from', from, to);
+    const diff = (to - from).toFixed(2);
+    if (Math.abs(diff) > 1) {
+      throw new Error('diff should be less or equal  1');
+    }
+    const leftPos = Math.floor(Math.min(from, to));
+    const rightPos = Math.ceil(Math.max(from, to));
+    const [leftVPos, rightVPos] = [leftPos, rightPos].map(pos => this.getValidPos(pos));
+    this._stopLast([leftVPos, rightVPos]);
+    this._renders[leftVPos].move(leftPos - from, leftPos - to);
+    this._renders[rightVPos].move(rightPos - from, rightPos - to);
+    this.pos = this.getValidPos(to);
+  }
+  _stopLast(itemsIndex$) {
+    for (const index of (this._lastShowItemsIndex$ || [])) {
+      this._renders[index].hide();
+    }
+    this._lastShowItemsIndex$ = itemsIndex$;
+  }
 }
 
-function cssUpdate(dom, pos) {
-    dom.classList.toggle('animate', false);
-    dom.style.transform = `translateX(${pos})`;
+class CarouselItemRender {
+  constructor(el, zeroPos) {
+    this.el = el;
+    this.zeroPos = zeroPos;
+  }
+  show(pos) {
+    this._tlStop();
+    this._toggleShow(true);
+    this.el.style.transform = this._posToStyle(pos);
+  }
+  hide() {
+    this._tlStop();
+    this._toggleShow(false);
+  }
+  /**
+   * 
+   * @param {*} from 为0时在视窗显示，随着数字增加向右移动
+   * @param {*} to 
+   */
+  move(from, to) {
+    this._tlStop();
+    this._toggleShow(true);
+    this._tlPlay(duration, { transform: [from, to].map(pos => this._posToStyle(pos)) });
+  }
+  _toggleShow(isShow) {
+    this.el.style.visibility = isShow ? '' : 'hidden';
+  }
+  _posToStyle(pos) {
+    return `translateX(${(pos + this.zeroPos) * 100}%)`;
+  }
+  _tlPlay(duration, style) {
+    const tl = this._tl = new TimeLine();
+    tl.addAnimation(new StyleAnimation(this.el, 0, duration, style, ease));
+    tl.play();
+  }
+  _tlStop() {
+    const { _tl } = this;
+    if (_tl) {
+      _tl.pause();
+      this._tl = null;
+    }
+  }
 }
+
 
 function log(...args) {
-    console.log.apply(null, args);
+  console.log.apply(null, args);
 }
