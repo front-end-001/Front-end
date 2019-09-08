50e6454bfe17295e737782e38242032b3ba2b56a
Hushin <874797577@qq.com>
2019-09-08 10:05:09 +0800

    组件封装

---
+++ 组件开发项目/id_1908051/webpack2/script.js
@@ -0,0 +1,24 @@
+import {Carousel} from "../component/component.js";
+
+function myCreate(Class, attributes){
+    let params = []
+    for(let name in attributes)
+    // object.setAttribute(name, attributes[name]);
+    params.push(attributes[name])
+    console.log(params);
+    var object = new Class(...params);
+    return object; 
+}
+let data = [
+    'https://static001.geekbang.org/resource/image/bb/21/bb38fb7c1073eaee1755f81131f11d21.jpg',
+    'https://static001.geekbang.org/resource/image/1b/21/1b809d9a2bdf3ecc481322d7c9223c21.jpg',
+    'https://static001.geekbang.org/resource/image/b6/4f/b6d65b2f12646a9fd6b8cb2b020d754f.jpg',
+    'https://static001.geekbang.org/resource/image/73/e4/730ea9c393def7975deceb48b3eb6fe4.jpg'
+]
+
+let container = document.body;
+<Carousel 
+    container={container}
+    imgUrls={data}
+></Carousel>
+c.appendTo(document.body);
