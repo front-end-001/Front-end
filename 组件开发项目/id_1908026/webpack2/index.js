e6e7a99563c1298fd0c8c2a58382da74edf200d0
alfredolee <alfredolee94@163.com>
2019-09-15 23:55:50 +0800

    Carousel完善以及Tab和Scroll

---
+++ 组件开发项目/id_1908026/webpack2/index.js
@@ -0,0 +1,10 @@
+import { Carousel } from "./components/carousel";
+
+function myCreate(Class, attributes) {
+  var object = new Class();
+  for (let name in attributes) object.setAttribute(name, attributes[name]);
+  return object;
+}
+
+var c = <Carousel width="100"></Carousel>;
+c.appendTo(document.body);
