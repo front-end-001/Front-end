cfbcdcb868562940296a389b44dd9e4285f4601b
unknown <173325505@qq.com>
2019-09-26 21:12:21 +0800

    提交

--- 有好货页面/id_1908045/src/index.js
+++ 有好货页面/id_1908045/src/index.js
@@ -7,7 +7,11 @@ import './style/style.less'
 function myCreate(Class, Attributes, ...children) {
     let object = new Class()
     for (let name in Attributes) {
-        object.setAttribute(name, Attributes[name])
+        if(name.match(/^on-([\s\S]+)$/)){
+            object.addEventListener(RegExp.$1, Attributes[name])
+        }else{
+            object.setAttribute(name, Attributes[name])
+        }
     }
     for (let child of children) {
         if (typeof child === 'string') {
@@ -19,8 +23,134 @@ function myCreate(Class, Attributes, ...children) {
     return object
 }
 
+function loadMore(e){
+    console.log(e)
+    this.setAttribute('placeHolderText','加载中，请稍后...')
+    setTimeout(()=>{
+        this.setAttribute('placeHolderText','没有更多了')
+    },1000)
+}
+
+function refresh(cb){
+    setTimeout(()=>{
+        cb()
+    },1000)
+}
+
 let tab = <TabView style="width: 100%; height: 100%">
-    <ScrollView tab-title="标题" style="background: lightpink">
+    <ScrollView tab-title="推荐" on-scrollToBottom={loadMore} placeHolderText='加载中，请稍后' on-refresh={refresh} style="background: lightpink">
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
+        abc abc abc abc abc abc abc abc abc abc
         abc abc abc abc abc abc abc abc abc abc
         abc abc abc abc abc abc abc abc abc abc
         abc abc abc abc abc abc abc abc abc abc
@@ -29,8 +159,8 @@ let tab = <TabView style="width: 100%; height: 100%">
         abc abc abc abc abc abc abc abc abc abc
         abc abc abc abc abc abc abc abc abc abc
     </ScrollView>
-    <ScrollView tab-title="标题2" style="background: lightgreen"></ScrollView>
-    <ScrollView tab-title="标题3" style="background: lightblue"></ScrollView>
+    <ScrollView tab-title="有趣的店" style="background: lightgreen"></ScrollView>
+    <ScrollView tab-title="品牌新店" style="background: lightblue"></ScrollView>
 </TabView>
 
 tab.appendTo(document.body)
