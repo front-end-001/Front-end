dde97d909c1ee6fa19d4da1f24a386949cedc442
lcl22hope <lcl22hope@gmail.com>
2019-09-26 23:49:08 +0800

    best good teacher logic

--- 有好货页面/id_1908010/week07/Text.js
+++ 有好货页面/id_1908010/week07/Text.js
@@ -1,15 +1,19 @@
-
 const PROPERTY_SYMBOL = Symbol("property");
 const ATTRIBUTE_SYMBOL = Symbol("attribute");
 const EVENT_SYMBOL = Symbol("event");
 const STATE_SYMBOL = Symbol("state");
-class Carousel {
+
+export default class Text {
     constructor(config){
         this[PROPERTY_SYMBOL] = Object.create(null);
         this[ATTRIBUTE_SYMBOL] = Object.create(null);
         this[EVENT_SYMBOL] = Object.create(null);
         this[STATE_SYMBOL] = Object.create(null);
         
+        this.text = config || "";
+
+        this[PROPERTY_SYMBOL].children = [];
+
         this.created();
     }
 
@@ -19,17 +23,12 @@ class Carousel {
     }
 
     created(){
-        this.root = document.createElement("div");
-        this.root.style.width = "300px";
-        this.root.style.height = "300px";
+        this.root = document.createElement("span");
+        this.root.innerText = this.text;
         this[STATE_SYMBOL].h = 0;
-        this.root.style.backgroundColor = `hsl(${this[STATE_SYMBOL].h}, 100%, 50%)`;
     }
     mounted(){
-        this.root.addEventListener("click", () => {
-            this[STATE_SYMBOL].h += 60;
-            this.root.style.backgroundColor = `hsl(${this[STATE_SYMBOL].h}, 60%, 70%)`;
-        })
+
     }
     unmounted(){
 
@@ -38,24 +37,24 @@ class Carousel {
 
     }
 
-    
-    log(){
-        console.log("width:", this.width);
+    appendChild(child){
+        this.children.push(child);
+        child.appendTo(this.root);
     }
-    get width(){
-        return this[PROPERTY_SYMBOL].width;
-    }
-    set width(value){
-        console.log("property change");
-        return this[PROPERTY_SYMBOL].width = value;
+
+
+    get children(){
+        return this[PROPERTY_SYMBOL].children;
     }
     getAttribute(name){
+        if(name == "style") {
+            return this.root.getAttribute("style");
+        }
         return this[ATTRIBUTE_SYMBOL][name]
     }
     setAttribute(name, value){
-        if(name == "width") {
-            this.width = value;
-            console.log("attibute change");
+        if(name == "style") {
+            this.root.setAttribute("style", value);
         }
         return this[ATTRIBUTE_SYMBOL][name] = value;
     }
