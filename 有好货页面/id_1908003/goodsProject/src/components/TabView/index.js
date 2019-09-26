4a26b68145e05caa2bcb7a39703658e4cb17e94f
Quanquan <zhanglinling0926@gmail.com>
2019-09-26 23:13:25 +0800

    feat(goods): 添加 current 逻辑初稿

--- 有好货页面/id_1908003/goodsProject/src/components/TabView/index.js
+++ 有好货页面/id_1908003/goodsProject/src/components/TabView/index.js
@@ -23,13 +23,31 @@ export default class Tab extends Component {
       </div>
     </div>;
 
+    if (this[ATTR_SYMBOL].current) {
+      this[STATUS_SYMBOL].current = parseInt(this[ATTR_SYMBOL].current, 10);
+    } else {
+      this[STATUS_SYMBOL].current = children[0].key;
+    }
     return tab;
   }
 
   /**
    * 设置当前激活 tab 项
    */
-  setCurrent() {}
+  setCurrent(key) {
+    // 执行子项展示逻辑
+    // 暂不考虑动画过程中的当前项判断, 使用 css 逻辑实现切换动画
+
+    // 修改 current 状态
+    this[STATUS_SYMBOL].current = key;
+  }
+
+  /**
+   * 获取当前激活项
+   */
+  get current() {
+    return this[STATUS_SYMBOL].current;
+  }
 
   attrInterceptor(name, value) {
     if (name === 'style') {
