527c86fca508e3aa55d1f82f4a02d5bb85478a2d
周毅 <zhouy@suirui.com>
2019-10-10 21:49:47 +0800

    week08

--- 有好货页面/id_1908013/index.js
+++ 有好货页面/id_1908013/index.js
@@ -1,173 +1,25 @@
 import TabView from './TabView'
-import Text from './Text'
 import ScrollView from './ScrollView'
-import Div from './Div'
+import ListView from './ListView'
 
-function myCreate(Class, attributes, ...children) {
-    var object = new Class();
-    for (let name in attributes) {
-        if (name.match(/^on-([\s\S]+)$/)) {
-            object.addEventListener(RegExp.$1, attributes[name])
-        } else {
-            object.setAttribute(name, attributes[name]);
-        }
-    }
-
-    for (let child of children) {
-        if (typeof child === 'string') {
-            object.appendChild(new Text(child))
-        } else {
-            object.appendChild(child)
-        }
-    }
-    return object;
-}
+import create from './create.js'
 
 function loadMore () {
     console.log('load more')
 }
-var c = <TabView style="width:100%;height:100%;overflow:hidden;display:flex;flex-direction:column">
-    <ScrollView tab-title="推荐" on-scrollToBottom={loadMore} style="-webkit-overflow-scrolling:touch;overflow:scroll;overflow-x:hidden;background-color:lightblue;white-space:normal;font-size:50px">
-        <Div className={'title'}>超多人收藏的店！</Div>
-        <Div>345</Div>
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgf
-        dgfdgdfgdfgfgdgfdgfdgdfgdfgfgdgf
-    </ScrollView>
-    <ScrollView tab-title="有趣的店" style="background-color:pink"></ScrollView>
-    <ScrollView tab-title="品牌新店" style="background-color:lightgreen"></ScrollView>
-</TabView>
-c.appendTo(document.body);
+
+window.render = function (obj, container) {
+    var c = <TabView style="width:100%;height:100%;overflow:hidden;display:flex;flex-direction:column">
+        <ScrollView tab-title="推荐" on-scrollToBottom={loadMore} style="-webkit-overflow-scrolling:touch;overflow:scroll;overflow-x:hidden;background-color:#eee;white-space:normal;background:url('./images/bg.png') center top no-repeat;background-size:100%">
+            <ListView data={obj}/>
+        </ScrollView>
+        <ScrollView tab-title="有趣的店" style="background-color:pink">
+
+        </ScrollView>
+        <ScrollView tab-title="品牌新店" style="background-color:lightgreen">
+
+        </ScrollView>
+    </TabView>
+    c.appendTo(container);
+}
+
