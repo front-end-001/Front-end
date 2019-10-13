5c4454f6b908d1390b52358667d3198b76c8c8ab
Quanquan <zhanglinling0926@gmail.com>
2019-10-13 11:08:48 +0800

    feat(goods):

--- 有好货页面/id_1908003/goodsProject/src/index.js
+++ 有好货页面/id_1908003/goodsProject/src/index.js
@@ -1,76 +1,41 @@
 import 'normalize.css';
 import './assets/style.css';
 // import './views/test';
-import Tab from './components/TabView';
+
+// 组件体系必备
 import createComponent from './components/createComponent';
+import Tab from './components/TabView';
 import TabItem from './components/TabItem';
 import ScrollView from './components/ScrollView';
+import ListView from './components/ListView';
 
-const indexHtml = (<Tab>
-  <div>测试</div>
-  <TabItem tab-title="有趣的店">
-    <ScrollView>
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    我是 TabItem1<br />
-    </ScrollView>
-  </TabItem>
-  <TabItem tab-title="每日推荐">我是 TabItem2</TabItem>
-</Tab>);
-
-const app = document.getElementById('app');
-indexHtml.appendTo(app);
+fetch('/static/data/commendTab.json').then(res => res.json()).then((data) => {
+  const indexHtml = (<Tab>
+    <TabItem tab-title="推荐">
+      <ScrollView>
+        <div data-list="轮播图数据">轮播图</div>
+        <div data-list="轮播图数据">双图推荐</div>
+        <ListView list-data="123">
+          我是 推荐<br />
+        </ListView>
+      </ScrollView>
+    </TabItem>
+    <TabItem tab-title="有趣的店">
+      <ScrollView>
+        <ListView list-data="123">
+          我是 有趣的店<br />
+        </ListView>
+      </ScrollView>
+    </TabItem>
+    <TabItem tab-title="品牌新店">
+      <ScrollView>
+        <ListView list-data="123">
+          我是 品牌新店<br />
+        </ListView>
+      </ScrollView>
+    </TabItem>
+  </Tab>);
+  console.log(data);
+  const app = document.getElementById('app');
+  indexHtml.appendTo(app);
+});
