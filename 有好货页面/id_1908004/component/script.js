6dd5246a180d5da2251d85034cdb94df456334b9
Castiel3399 <39975883+Castiel3399@users.noreply.github.com>
2019-10-24 21:00:59 +0800

    Revert "004-09week"
    This reverts commit e3718add0136f2f0460dc7b5fd31ef2f45eaf8bd.

--- 有好货页面/id_1908004/component/script.js
+++ 有好货页面/id_1908004/component/script.js
@@ -5,8 +5,11 @@ import ListView from "./ListView.js"
 import Div from "./Div.js"
 import {create} from "./create.js"
 
+<<<<<<< HEAD
 import tree from "./my.component";
 
+=======
+>>>>>>> 76f8f1a5d637205ab4509d9c71d49783b2f895d4
 
 function loadMore(){
     setTimeout(()=>{
@@ -15,10 +18,47 @@ function loadMore(){
 }
 
 window.render = function(data, root){
-    var c = <div>
-        <span class="x">abc</span>
-        <ListView style="abc:1" data={[{a:1, b:2}]}></ListView>
-    </div>
+<<<<<<< HEAD
+    var c = tree;
+=======
+    var c = <TabView style="width:100%;height:100%;">
+        <ScrollView tab-title="推荐" placeHolderText="load more" on-scrolToBottom={loadMore} style="-webkit-overflow-scrolling:touch;overflow:scroll;background-color:lightblue;white-space:normal;font-size:50px">
+            <ListView data={data}></ListView>
+        </ScrollView>
+        <ScrollView tab-title="有趣的店"  style="background-color:lightgreen;">
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+        def def def def def def def def def def def def def def def def def def 
+
+        </ScrollView>
+        <ScrollView tab-title="品牌新店" style="background-color:pink;"></ScrollView>
+    </TabView>
+>>>>>>> 76f8f1a5d637205ab4509d9c71d49783b2f895d4
     c.appendTo(document.body);
 }
 
