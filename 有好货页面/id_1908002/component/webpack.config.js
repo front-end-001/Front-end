600c5b85058d30366adc13e2538b9c31e43f02d4
CharlesLeons <51524114+CharlesLeons@users.noreply.github.com>
2019-10-24 19:36:22 +0800

    002 - week 10

--- 有好货页面/id_1908002/component/webpack.config.js
+++ 有好货页面/id_1908002/component/webpack.config.js
@@ -22,7 +22,11 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
-            }
+            },
+            {
+                test: /\.css$/i,
+                use: [require.resolve('./component-css-loader.js')],
+            },
         ]
     },
     mode: "development",
