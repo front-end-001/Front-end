88fe6030cb783dfd5f7226f655ce08377252c250
MBearo <295290708@qq.com>
2019-12-04 12:22:56 +0800

    UPG

--- 有好货页面/id_1908020/new/test/testLoader.js
+++
@@ -1,6 +0,0 @@
-const path = require('path')
-const fs = require('fs-extra')
-const loader = require('../component-loader');
-
-const template = fs.readFileSync(path.resolve(__dirname, '../my.component'), 'utf8');
-loader(template);
--- 有好货页面/id_1908020/src3/index.js
+++
@@ -1,8 +0,0 @@
-import Jreact from './jreact/jreact.js'
-import JreactDOM from './jreact/jreact-dom.js'
-import App from './component/App.js'
-
-
-// window.JreactDOM = JreactDOM
-
-JreactDOM.render(<App/>, document.querySelector('#app'))
