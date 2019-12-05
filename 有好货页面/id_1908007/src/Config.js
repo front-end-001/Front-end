244f87bec1377135bef33872ea94a7891b5a708b
Lemon-Energy <343365473@qq.com>
2019-12-05 22:55:27 +0800

    变更

--- 有好货页面/id_1908007/src/Config.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("touchmove",function(e){ 
-    if(e.touches.length == 2) 
-        e.preventDefault(); 
-}, {passive:false});
-document.addEventListener("touchmove",function(e){ 
-    if(e.touches.length == 1) 
-        e.preventDefault(); 
-}, {passive:false});
--- 有好货页面/id_1908007/src/widgets/Divider.js
+++
@@ -1,9 +0,0 @@
-import Component from '../BaseComponent'
-import "./Divider.scss"
-
-export default class Divider extends Component {
-    constructor(props) {
-        super(props)
-        this.root.className = "divider"
-    }
-}
